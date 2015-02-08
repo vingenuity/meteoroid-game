ca92e22fa36c4947c472d38e06c89f27836a8e01
Vincent Kocks <howlingblue@gmail.com>
2015-02-07 23:14:57 -0600

    Added touchscreen input to Meteoroid.

--- Code/GameInputSystem.cpp
+++ Code/GameInputSystem.cpp
@@ -33,21 +33,31 @@ void GameInputSystem::OnUpdate( float /*deltaSeconds*/ )
 		if( controlledEntity == nullptr )
 			continue;
 
-		ConvertEulerAnglesToVector( controlledEntity->orientation, entityHeading );
 
 		Keyboard* keyboard = PeripheralInterface::GetKeyboard();
+		TouchScreen* touchscreen = PeripheralInterface::GetTouchscreen();
+		unsigned int numScreenTouches = touchscreen->GetNumberOfTouches();
+
+		if( numScreenTouches == 1 )
+		{
+			FloatVector3 touchPosition( touchscreen->GetTouchPointAtIndex( 0 ).location );
+			FloatVector3 nextEntityHeading = touchPosition - controlledEntity->position;
+			controlledEntity->orientation.yawDegreesAboutZ = ConvertRadiansToDegrees( ConvertVectorToAngleRadians( FloatVector2( nextEntityHeading.x, nextEntityHeading.y ) ) );
+		}
+
+		ConvertEulerAnglesToVector( controlledEntity->orientation, entityHeading );
+
 		controlledEntity->angularVelocity.yawDegreesAboutZ = ( keyboard->KeyIsPressedOrHeld( Keyboard::A ) - keyboard->KeyIsPressedOrHeld( Keyboard::D ) ) * 160.f;
+		controlledEntity->acceleration += entityHeading * ( ( keyboard->KeyIsPressedOrHeld( Keyboard::W ) + numScreenTouches ) * 20.f );
 
-		controlledEntity->acceleration += entityHeading * ( keyboard->KeyIsPressedOrHeld( Keyboard::W ) * 20.f );
-
-		if( keyboard->KeyIsPressed( Keyboard::S ) )
+		if( keyboard->KeyIsPressed( Keyboard::S ) || ( numScreenTouches == 2 ) )
 		{
 			EventDataBundle warpData;
 			warpData.SetParameter( STRING_1stEntity, controlledEntity );
 			EventCourier::SendEvent( EVENT_EngageWarp, warpData );
 		}
 
-		if( keyboard->KeyIsPressed( Keyboard::SPACEBAR ) )
+		if( keyboard->KeyIsPressed( Keyboard::SPACEBAR ) || ( touchscreen->FindNumberOfStartedTouches() == 1 ) )
 		{
 			EventDataBundle fireData;
 			fireData.SetParameter( STRING_1stEntity, controlledEntity );
